3f54122f2099616f418481ab9636ab0e1e68821c
Zihag <nghia22102002@gmail.com>
2025-05-21 00:02:18 +0700

    check is logged in

--- lib/app.dart
+++ lib/app.dart
@@ -9,12 +9,31 @@ import 'package:splitmate/features/presentation/pages/add_expense_screen.dart';
 import 'package:splitmate/features/presentation/pages/home_screen.dart';
 import 'package:splitmate/features/presentation/pages/sign_in_screen.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _isLoggedIn();
+  }
+
+  Future<bool> _isLoggedIn() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    User? user = FirebaseAuth.instance.currentUser;
+
+    return user != null;
+  }
+  @override
   Widget build(BuildContext context) {
-    final User? user = FirebaseAuth.instance.currentUser;
     return MultiBlocProvider(
       providers: [
         BlocProvider<AuthBloc>(
@@ -24,16 +43,14 @@ class App extends StatelessWidget {
           ),
         ),
         BlocProvider<ExpenseBloc>(
-        create: (_) => ExpenseBloc(
-        firestore: FirebaseFirestore.instance,
-        auth: FirebaseAuth.instance,
+          create: (_) => ExpenseBloc(
+            firestore: FirebaseFirestore.instance,
+            auth: FirebaseAuth.instance,
+          ),
         ),
-
-    ),
         BlocProvider(
           create: (_) => ExpenseBalanceBloc(FirebaseFirestore.instance),
         ),
-
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -41,10 +58,23 @@ class App extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.indigo,
         ),
-        home: SignInScreen(), // Gọi màn login ở đây
+        home: FutureBuilder<bool>(
+          future: _isLoggedIn(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            } else if (snapshot.hasData && snapshot.data == true) {
+              return const HomeScreen();
+            } else {
+              return const SignInScreen();
+            }
+          },
+        ),
         routes: {
-          '/home': (_) => HomeScreen(), // ✅ định nghĩa route ở đây
-          '/add-expense': (_) => AddExpenseScreen(), // ✅ định nghĩa route ở đây'
+          '/home': (_) => const HomeScreen(),
+          '/add-expense': (_) => const AddExpenseScreen(),
         },
       ),
     );
